7178b21e7dd9df080ebee79bf587142cdc7af132
Paulina <paulina.justyna.niedzielska@gmail.com>
2022-07-20 06:39:43 +0100

    working on game code

--- rock paper scissors/app.js
+++ rock paper scissors/app.js
@@ -16,11 +16,45 @@ const paper = document.getElementById("paper")
 const scissors = document.getElementById("scissors")
 const arrayOfChoices = [rock, paper, scissors]
 
-let playerChoice
 
-for (let i = 0; i < arrayOfChoices.length; i++){
- arrayOfChoices[i].addEventListener("click", ()=>{
+for (let i = 0; i < arrayOfChoices.length; i++) {
+ arrayOfChoices[i].addEventListener("click", () => {
+  let playerChoice
+  let computerChoice
+
+  //input player choice
   playerChoice = arrayOfChoices[i].id
+
+  // hide other option, so only choosen option will be shown
+  for (let y = 0; y < arrayOfChoices.length; y++) {
+   if (y !== i) {
+    arrayOfChoices[y].style.display = "none"
+   }
+  }
+
+  //randomise computer option
+  const randomNum = Math.ceil(Math.random() * 3)
+  if (randomNum === 1) { computerChoice = "rock" }
+  else if (randomNum === 2) { computerChoice = "paper" }
+  else if (randomNum === 3) { computerChoice = "scissors" }
+
+  //add img of computer choice
+  const computerDiv = document.querySelector(".computer")
+  computerDiv.style.display = "block"
+  const computerChoiceImg = document.querySelector(".computerChoiceImg")
+  if(computerChoice = "rock"){
+   computerChoiceImg.src = "/images/rock.svg"
+  } else if(computerChoice = "paper"){
+   computerChoiceImg.src = "/images/paper.svg"
+  }
+  else if(computerChoice = "scissors"){
+   computerChoiceImg.src = "/images/scissors.svg"
+  }
+
+  //display winner
+
+  
   console.log(playerChoice)
+  console.log(computerChoice)
  })
 }
